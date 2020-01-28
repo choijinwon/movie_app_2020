ccfa6c47effaa40ff9eae6b79880aa0b23b13998
최진원 <choejin-won@choejin-won-ui-MacBookPro.local>
2020-01-28 17:37:38 +0900

    #2.0 Creating you first React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+const foodILike =[
+  {
+    id:1,
+    name:"kimchi",
+    image:"http://image.auction.co.kr/itemimage/1b/45/1c/1b451c3fa6.jpg",
+    rating:4,
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  },
+  {
+    id:2,
+    name:"ramen",
+    image:"http://image.auction.co.kr/itemimage/1b/45/1c/1b451c3fa6.jpg",
+    rating:4.9,
+
+  },
+  {
+    id:3,
+    name:"samginopsal",
+    image:"http://image.auction.co.kr/itemimage/1b/45/1c/1b451c3fa6.jpg",
+    rating:5,
+  },
+  {
+    id:4,
+    name:"chukiumi",
+    image:"http://image.auction.co.kr/itemimage/1b/45/1c/1b451c3fa6.jpg",
+    rating:4.3,
+  }
+]
+function Food({name,priceImg,rating}){
+  return <div>
+      <h2>I like {name}</h2>
+      <img src={priceImg} alt={name}/>
+      <p>{rating}</p>
     </div>
-  );
 }
 
+Food.prototype ={
+  name:PropTypes.string.isRequired,
+  priceImg:PropTypes.string.isRequired,
+  rating:PropTypes.number
+}
+function App() {
+   return (
+    <div>
+     <h1>Hello</h1>
+      { foodILike.map(item => 
+        <Food 
+          key={item.id}
+          name={item.name} 
+          priceImg={item.image} 
+          rating={item.rating} 
+          />
+      )}
+    </div>
+   );
+  }
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function Header(){
+    return <h3>사랑합니다.</h3>
+}
+
+export default Header;
