7643cac2b4ae54e043fb6d4a43a1dff8e3d5514d
최진원 <choejin-won@choejin-won-ui-MacBookPro.local>
2020-01-29 10:58:02 +0900

    #3.0 life cycle  React App

--- src/App.js
+++ src/App.js
@@ -1,59 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-const foodILike =[
-  {
-    id:1,
-    name:"kimchi",
-    image:"http://image.auction.co.kr/itemimage/1b/45/1c/1b451c3fa6.jpg",
-    rating:4,
 
-  },
-  {
-    id:2,
-    name:"ramen",
-    image:"http://image.auction.co.kr/itemimage/1b/45/1c/1b451c3fa6.jpg",
-    rating:4.9,
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    console.log(props)
+  };
 
-  },
-  {
-    id:3,
-    name:"samginopsal",
-    image:"http://image.auction.co.kr/itemimage/1b/45/1c/1b451c3fa6.jpg",
-    rating:5,
-  },
-  {
-    id:4,
-    name:"chukiumi",
-    image:"http://image.auction.co.kr/itemimage/1b/45/1c/1b451c3fa6.jpg",
-    rating:4.3,
+  state ={
+    count:20
+  };
+  add=() =>{
+    this.setState(current =>
+      ({count:current.count+1}))
   }
-]
-function Food({name,priceImg,rating}){
+  Minus=() =>{
+    if(this.state.count ===0){
+      return false;
+    }else{
+      this.setState(current=>({count:current.count-1}))
+    }
+
+  }
+  componentDidMount(){
+    console.log("componentDidMount")
+  }
+  componentDidUpdate(){
+    console.log("componentDidUpdate")
+  }
+  componentWillUnmount(){
+    console.log("componentWillUnmount")
+  }
+  render(){
+  console.log("render")
   return <div>
-      <h2>I like {name}</h2>
-      <img src={priceImg} alt={name}/>
-      <p>{rating}</p>
-    </div>
-}
-
-Food.prototype ={
-  name:PropTypes.string.isRequired,
-  priceImg:PropTypes.string.isRequired,
-  rating:PropTypes.number
-}
-function App() {
-   return (
-    <div>
-     <h1>Hello</h1>
-      { foodILike.map(item => 
-        <Food 
-          key={item.id}
-          name={item.name} 
-          priceImg={item.image} 
-          rating={item.rating} 
-          />
-      )}
-    </div>
-   );
+    <h1> The Nume is {this.state.count}</h1>
+    <button onClick={this.add}>add</button>
+    <button onClick={this.Minus}>Minus</button>
+  </div>
   }
+
+}
 export default App;
