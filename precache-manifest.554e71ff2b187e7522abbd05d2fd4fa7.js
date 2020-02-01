708695cb6cbc3e1c220cc72469c0d01dfec37f38
최진원 <choejin-won@choejin-won-ui-MacBookPro.local>
2020-02-01 19:02:42 +0900

    Updates

---
+++ precache-manifest.554e71ff2b187e7522abbd05d2fd4fa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3784067297ceaf1e025aa4134a5d6d61",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a83ecd88ad2bb237593e",
+    "url": "/movie_app_2020/static/css/main.e3b4cfce.chunk.css"
+  },
+  {
+    "revision": "90a3237db90f8a26dd19",
+    "url": "/movie_app_2020/static/js/2.4607b1ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.4607b1ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a83ecd88ad2bb237593e",
+    "url": "/movie_app_2020/static/js/main.8e82826f.chunk.js"
+  },
+  {
+    "revision": "372b1ae89a0514312af3",
+    "url": "/movie_app_2020/static/js/runtime-main.3dfb44e2.js"
+  }
+]);
